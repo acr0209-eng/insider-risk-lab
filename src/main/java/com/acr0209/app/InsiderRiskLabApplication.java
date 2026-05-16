6258effb7fc511911bd0918f180ee6ca8d36921c
acr0209-eng <acr0209@gmail.com>
2026-05-16 10:24:59 +0900

    Add application entrypoint

---
+++ src/main/java/com/acr0209/app/InsiderRiskLabApplication.java
@@ -0,0 +1,11 @@
+package com.acr0209.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiderRiskLabApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InsiderRiskLabApplication.class, args);
+    }
+}
